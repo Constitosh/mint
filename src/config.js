0c980a27a41683484719e0c9c707c4723f0ac3d5
Mfer Vestside <97671389+Constitosh@users.noreply.github.com>
2025-10-15 16:35:27 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,19 +1,22 @@
 import 'dotenv/config';
 
-export const CFG = {
+export const CONFIG = {
+  network: process.env.NETWORK || 'Mainnet',
   blockfrostKey: process.env.BLOCKFROST_KEY,
-  price: BigInt(process.env.PRICE || "30000000"),
-  receiveAddress: process.env.RECEIVE_ADDRESS,
-
+  seedPhrase: process.env.SEED_PHRASE,
+  mintAddress: process.env.MINT_ADDRESS,
+  priceAda: Number(process.env.PRICE_ADA || 30),
+  port: Number(process.env.PORT || 3005),
+  pollInterval: Number(process.env.POLL_INTERVAL || 8),
   tddPolicyId: process.env.TDD_POLICY_ID,
-  tddScriptPath: process.env.TDD_POLICY_SCRIPT_JSON_PATH,
-  tddSkeyPath: process.env.TDD_POLICY_SKEY_PATH,
-
   trixPolicyId: process.env.TRIX_POLICY_ID,
-  trixScriptPath: process.env.TRIX_POLICY_SCRIPT_JSON_PATH,
-  trixSkeyPath: process.env.TRIX_POLICY_SKEY_PATH,
 
-  mintWalletSkeyPath: process.env.MINT_WALLET_SKEY_CBOR,
-
-  pollMs: Number(process.env.POLL_MS || 6000),
+  paths: {
+    tddPolicy: './policies/tdd/policy.script',
+    trixPolicy: './policies/trix2056/policy.script',
+    tddJson: './data/combined_metadata_TDD.json',
+    trixJson: './data/combined_metadata_2056.json',
+    collectionDescriptions: './data/collection_descriptions.json',
+    sqlite: './data/mint.sqlite',
+  },
 };
