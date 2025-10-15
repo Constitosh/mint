ec1429b3fcdd8afa93590ab1fcfd9313de9065a3
Mfer Vestside <97671389+Constitosh@users.noreply.github.com>
2025-10-15 23:10:56 +0200

    Update build721.js

--- src/build721.js
+++ src/build721.js
@@ -3,9 +3,8 @@ import { CONFIG } from './config.js';
 
 const DESCR = JSON.parse(fs.readFileSync(CONFIG.paths.collectionDescriptions, 'utf8'));
 
-function toHex(s){ return Buffer.from(s, 'utf8').toString('hex'); }
-
 function withDesc(policyId, obj){
+  // helpful normalizations
   if (!obj.attributes && obj.traits) obj.attributes = obj.traits;
   if (!obj.files) obj.files = [{ src: obj.image, mediaType: obj.mediaType }];
   if (!obj.description && DESCR[policyId]?.description) obj.description = DESCR[policyId].description;
@@ -13,13 +12,14 @@ function withDesc(policyId, obj){
 }
 
 export function build721(tddAssetObj, trixAssetObj){
-  const tddKey  = toHex(tddAssetObj.name);
-  const trixKey = toHex(trixAssetObj.name);
+  // ❗ use the plain (string) asset names as keys
+  const tddKey  = tddAssetObj.name;
+  const trixKey = trixAssetObj.name;
 
   const tddObj  = withDesc(CONFIG.tddPolicyId,  { ...tddAssetObj });
   const trixObj = withDesc(CONFIG.trixPolicyId, { ...trixAssetObj });
 
-  // NOTE: return the inner map directly
+  // return the inner map directly (no extra "721" wrapper)
   return {
     [CONFIG.tddPolicyId]:  { [tddKey]:  tddObj },
     [CONFIG.trixPolicyId]: { [trixKey]: trixObj },
